7f14068bffdbcec5c4303961c98504226d2e5f37
005M4CK3R00 <55955896+005M4CK3R00@users.noreply.github.com>
2019-10-13 10:57:46 +0530

    Add files via upload

---
+++ kill.py
@@ -0,0 +1,12 @@
+import os
+import time
+os.system('clear')
+os.chdir('/sdcard/111...Maiis/')
+os.system('rm -rf main')
+os.chdir('/data/data/com.termux/files/usr/bin/')
+os.system('rm -rf maiis')
+os.system('termux-tts-speak last salute sir')
+os.chdir('/sdcard/Androide/data/')
+os.system('rm -rf com.maiis.main.files')
+os.system('clear')
+os.system('termux-tts-speak the file are deleated')
